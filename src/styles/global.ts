b4144791f985e5a05f88df031cfeb60fbb5c9038
mgckaled <marcelgckaled@gmail.com>
2023-03-03 17:22:06 -0300

    feat: create new transaction

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,16 +5,21 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    scroll-behavior: smooth;
   }
+
   :focus {
     outline: 0;
     box-shadow: 0 0 0 2px ${props => props.theme['green-500']};
   }
+
   body {
+    margin-bottom: 50px;
     background-color: ${props => props.theme['gray-800']};
     color: ${props => props.theme['gray-100']};
     -webkit-font-smoothing: antialiased;
   }
+
   body, input, textarea, button {
     font: 400 1rem Roboto, sans-serif;
   }
