f1065f191423bc680e8afffcb7aad35191285824
mgckaled <marcelgckaled@gmail.com>
2023-03-09 09:41:50 -0300

    feat: apply useMemo in useSummary hook

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -61,7 +61,7 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
 
   useEffect(() => {
     fetchTransactions()
-  }, [])
+  }, [fetchTransactions])
 
   return (
     <TransactionsContext.Provider
